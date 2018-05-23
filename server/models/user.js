42650034b23375c13183ead834e826d222a6949e
Benjamin Huang <bhuang@rms.com>
2018-05-22 17:41:58 -0700

    start working on auth

--- server/models/user.js
+++ server/models/user.js
@@ -4,16 +4,43 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  age: {
-    type: Number,
-    required: true,
-    default: 30
-  }
+const validator = require("validator");
 
+// create a mongoose model
+var User = mongoose.model("User", {
+  email: {
+    type: String,
+    required: true,
+    minlength: 3,
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: `{VALUE} is not a valid email`
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    trim: true,
+    validate: {
+      validator: validator.isEmail,
+      message: `{VALUE} is not a valid email`
+    }
+  },
+  tokens: [
+    {
+      access: {  // the token type
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
-module.exports = mongoose.model('User', userSchema);
+
+module.exports = { User };
